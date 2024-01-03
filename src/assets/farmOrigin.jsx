afa9b80a94c3fc4a4323c48675d782ccf32b4107
dveloper-Elisa <kwizeraelisa77@gmail.com>
2024-01-03 11:56:43 +0200

    ammend

--- src/assets/farmOrigin.jsx
+++ src/assets/farmOrigin.jsx
@@ -1,17 +1,37 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Button from "../components/button";
 
 const FarmOrigin = () => {
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    INharvestareachoose: "",
+    INharvestmanagementchoose: "",
+    INharvestsaledocumentchoose: "",
+    INdocumentkeepchoose: "",
+    INharvestscalechoose: "",
+    Ppatternerchoose: "",
+  });
+
+  const handleRadioChange = (question, value) => {
+    setFormData({ ...formData, [question]: value });
+  };
+
   const navigateFunction = () => {
-    navigate("/farmingtu");
+    const allInputs = Object.values(formData).every((value) => value !== "");
+    if (allInputs) {
+      navigate("/farmingtu");
+    } else {
+      alert("Please answer all questions before continuing!");
+    }
   };
 
   const navigateBack = () => {
     navigate("/farmingrule");
   };
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col items-center gap-4">
       <strong className="text-[25px]">
         <span className="text-center text-[#166534] ">
           <b id="drop" className="hover:cursor-pointer max-w-fit text-black">
@@ -37,15 +57,39 @@ const FarmOrigin = () => {
               </label>
               <p className="flex gap-4 font-bold my-2">
                 <span>
-                  <input type="radio" name="INharvestareachoose" id="ab" />
+                  <input
+                    type="radio"
+                    value="yego"
+                    name="INharvestareachoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestareachoose", "yego")
+                    }
+                    id="ab"
+                  />
                   <label htmlFor="ab">Yego</label>
                 </span>
                 <span>
-                  <input type="radio" name="INharvestareachoose" id="bb" />
+                  <input
+                    type="radio"
+                    value="oya"
+                    name="INharvestareachoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestareachoose", "oya")
+                    }
+                    id="bb"
+                  />
                   <label htmlFor="bb">Oya</label>
                 </span>
                 <span>
-                  <input type="radio" name="INharvestareachoose" id="cb" />
+                  <input
+                    type="radio"
+                    value="ntibikorwa"
+                    name="INharvestareachoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestareachoose", "ntibikorwa")
+                    }
+                    id="cb"
+                  />
                   <label htmlFor="cb">Ntibikorwa</label>
                 </span>
               </p>
@@ -77,7 +121,11 @@ const FarmOrigin = () => {
                 <span>
                   <input
                     type="radio"
+                    value={"yego"}
                     name="INharvestmanagementchoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestmanagementchoose", "yego")
+                    }
                     id="ac"
                   />
                   <label htmlFor="ac">Yego</label>
@@ -85,15 +133,26 @@ const FarmOrigin = () => {
                 <span>
                   <input
                     type="radio"
+                    value={"oya"}
                     name="INharvestmanagementchoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestmanagementchoose", "oya")
+                    }
                     id="bc"
                   />
-                  <label htmlFor="bb">Oya</label>
+                  <label htmlFor="bc">Oya</label>
                 </span>
                 <span>
                   <input
                     type="radio"
+                    value={"ntibikorwa"}
                     name="INharvestmanagementchoose"
+                    onChange={() =>
+                      handleRadioChange(
+                        "INharvestmanagementchoose",
+                        "ntibikorwa"
+                      )
+                    }
                     id="cc"
                   />
                   <label htmlFor="cc">Ntibikorwa</label>
@@ -123,7 +182,11 @@ const FarmOrigin = () => {
                 <span>
                   <input
                     type="radio"
+                    value={"yego"}
                     name="INharvestsaledocumentchoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestsaledocumentchoose", "yego")
+                    }
                     id="ad"
                   />
                   <label htmlFor="ad">Yego</label>
@@ -131,7 +194,11 @@ const FarmOrigin = () => {
                 <span>
                   <input
                     type="radio"
+                    value={"oya"}
                     name="INharvestsaledocumentchoose"
+                    onChange={() =>
+                      handleRadioChange("INharvestsaledocumentchoose", "oya")
+                    }
                     id="bd"
                   />
                   <label htmlFor="bd">Oya</label>
@@ -139,7 +206,14 @@ const FarmOrigin = () => {
                 <span>
                   <input
                     type="radio"
+                    value={"ntibikorwa"}
                     name="INharvestsaledocumentchoose"
+                    onChange={() =>
+                      handleRadioChange(
+                        "INharvestsaledocumentchoose",
+                        "ntibikorwa"
+                      )
+                    }
                     id="cd"
                   />
                   <label htmlFor="cd">Ntibikorwa</label>
@@ -168,15 +242,39 @@ const FarmOrigin = () => {
               </label>
               <p className="flex gap-4 font-bold my-2">
                 <span>
-                  <input type="radio" name="INdocumentkeepchoose" id="ae" />
+                  <input
+                    type="radio"
+                    value={"yego"}
+                    name="INdocumentkeepchoose"
+                    onChange={() =>
+                      handleRadioChange("INdocumentkeepchoose", "yego")
+                    }
+                    id="ae"
+                  />
                   <label htmlFor="ae">Yego</label>
                 </span>
                 <span>
-                  <input type="radio" name="INdocumentkeepchoose" id="be" />
+                  <input
+                    type="radio"
+                    value={"oya"}
+                    name="INdocumentkeepchoose"
+                    onChange={() =>
+                      handleRadioChange("INdocumentkeepchoose", "oya")
+                    }
+                    id="be"
+                  />
                   <label htmlFor="be">Oya</label>
                 </span>
                 <span>
-                  <input type="radio" name="INdocumentkeepchoose" id="ce" />
+                  <input
+                    type="radio"
+                    value={"ntibikorwa"}
+                    name="INdocumentkeepchoose"
+                    onChange={() =>
+                      handleRadioChange("INdocumentkeepchoose", "ntibikorwa")
+                    }
+                    id="ce"
+                  />
                   <label htmlFor="ce">Ntibikorwa</label>
                 </span>
               </p>
@@ -203,15 +301,39 @@ const FarmOrigin = () => {
               </label>
               <p className="flex gap-4 font-bold my-2">
                 <span>
-                  <input type="radio" name="INharvestscalechoose" id="af" />
+                  <input
+                    type="radio"
+                    value={"yego"}
+                    onChange={() =>
+                      handleRadioChange("INharvestscalechoose", "yego")
+                    }
+                    name="INharvestscalechoose"
+                    id="af"
+                  />
                   <label htmlFor="af">Yego</label>
                 </span>
                 <span>
-                  <input type="radio" name="INharvestscalechoose" id="bf" />
+                  <input
+                    type="radio"
+                    value={"oya"}
+                    onChange={() =>
+                      handleRadioChange("INharvestscalechoose", "oya")
+                    }
+                    name="INharvestscalechoose"
+                    id="bf"
+                  />
                   <label htmlFor="bf">Oya</label>
                 </span>
                 <span>
-                  <input type="radio" name="INharvestscalechoose" id="cf" />
+                  <input
+                    type="radio"
+                    value={"ntibikorwa"}
+                    onChange={() =>
+                      handleRadioChange("INharvestscalechoose", "ntibikorwa")
+                    }
+                    name="INharvestscalechoose"
+                    id="cf"
+                  />
                   <label htmlFor="cf">Ntibikorwa</label>
                 </span>
               </p>
@@ -251,15 +373,35 @@ const FarmOrigin = () => {
             </label>
             <p className="flex gap-4 font-bold my-2">
               <span>
-                <input type="radio" name="Ppatternerchoose" id="ai" />
+                <input
+                  type="radio"
+                  name="Ppatternerchoose"
+                  value={"yego"}
+                  onChange={() => handleRadioChange("Ppatternerchoose", "yego")}
+                  id="ai"
+                />
                 <label htmlFor="ai">Yego</label>
               </span>
               <span>
-                <input type="radio" name="Ppatternerchoose" id="bi" />
+                <input
+                  type="radio"
+                  name="Ppatternerchoose"
+                  value={"oya"}
+                  onChange={() => handleRadioChange("Ppatternerchoose", "oya")}
+                  id="bi"
+                />
                 <label htmlFor="bi">Oya</label>
               </span>
               <span>
-                <input type="radio" name="Ppatternerchoose" id="ci" />
+                <input
+                  type="radio"
+                  name="Ppatternerchoose"
+                  value={"ntibikorwa"}
+                  onChange={() =>
+                    handleRadioChange("Ppatternerchoose", "ntibikorwa")
+                  }
+                  id="ci"
+                />
                 <label htmlFor="ci">Ntibikorwa</label>
               </span>
             </p>
@@ -274,8 +416,13 @@ const FarmOrigin = () => {
           </div>
         </div>
       </div>
-      <div className="flex gap-2">
-        <Button onClick={navigateBack} text={"Previouse"} />
+      <div className="flex gap-2 w-[37%] my-5 sm:flex sm:flex-col md:flex md:flex-row lg:flex">
+        <Button
+          onClick={() => {
+            navigateBack();
+          }}
+          text={"Previouse"}
+        />
         <Button onClick={navigateFunction} text={"Next"} />
       </div>
     </div>
